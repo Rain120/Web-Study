aa83e98cde6247b211a0419a26a5e303697a521a
Rain120 <1085131904@qq.com>
2018-06-11 17:22:38 +0800

    Create 排序二叉树的实现.js

---
+++ 排序二叉树的实现.js
@@ -0,0 +1,32 @@
+/*
+ * 排序二叉树的实现
+ */
+function BinaryTree() {
+	var Node = function(key) {
+		this.key = key;
+		this.left = null;
+		this.right = null;
+    }
+
+	var root = null;
+	
+	var insertNode = function(node, newNode) {
+		return node.key > newNode.key ? 
+(node.left ? insertNode(node.left, newNode) : node = newNode) : 
+(node.right ? insertNode(node.right, newNode) : node = newNode);
+    }
+
+	this.insert = function(key) {
+		var newNode = new Node(key);
+		if (root) {
+			insertNode(root, newNode);
+		} else {
+			root = newNode;
+        }
+	}
+}
+var nodes = [8, 3, 10, 1, 6, 14, 4, 7, 13];
+var binaryTree = new BinaryTree();
+nodes.forEach((key) =>{
+	binaryTree.insert(key);
+})
