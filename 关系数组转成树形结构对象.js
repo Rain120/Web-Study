8f80774fdeac129bb446a723d79e484ed9267425
Rain120 <1085131904@qq.com>
2018-08-17 11:34:57 +0800

    Create 关系数组转成树形结构对象.js

---
+++ 关系数组转成树形结构对象.js
@@ -0,0 +1,46 @@
+/**
+之前面试依图等好几个公司遇到的这个问题
+obj = [
+    {id:1,parent:null},
+    {id:2,parent:1},
+    {id:3,parent:2}
+]
+     ||
+     ||
+     VV
+obj2 = {
+    obj:{
+        id: 1,
+        parent: null,
+        child: {
+            id: 2,
+            parent: 1,
+            child: {
+                id: ,3,
+                parent: 2
+            }
+        }
+    }
+}
+**/
+
+var obj = [
+    {id:3,parent:2},
+    {id:1,parent:null},
+    {id:2,parent:1},
+]
+obj.map(item => {
+	if (item.parent !== null) {
+        obj.map(o => {
+			if (item.parent === o.id) {
+				if (!o.child) {
+					o.child = [];
+				}
+				o.child.push(item);
+				o.child = o.child;
+            }
+        });
+    }
+});
+obj = obj.filter(item => item.parent === null)[0]
+console.log(obj)
