1bfedc690f325a35871e4f6311adc781acf674b8
Rain120 <1085131904@qq.com>
2019-04-09 17:17:17 +0800

    Update 获取DOM的offset的值.js

--- 获取DOM的offset的值.js
+++ 获取DOM的offset的值.js
@@ -4,13 +4,7 @@ function getOffset(el) {
     while(el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
         _x += el.offsetLeft - el.scrollLeft;
         _y += el.offsetTop - el.scrollTop;
-        // chrome/safari
-        if ($.browser.webkit) {
-            el = el.parentNode;
-        } else {
-            // firefox/IE
-            el = el.offsetParent;
-        }
+        el = el.offsetParent;
     }
     return {
       top: _y,
