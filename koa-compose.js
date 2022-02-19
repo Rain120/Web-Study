c9ec0d7757431cfd9fc8dc428d5aa7aae247a1e2
Rain120 <chao1202016@outlook.com>
2022-02-19 13:03:39 +0800

    Create koa-compose.js

---
+++ koa-compose.js
@@ -0,0 +1,46 @@
+// 洋葱🧅模型
+function compose(middleware) {
+  if (!Array.isArray(middleware)) {
+    throw new TypeError('Middleware stack must be an array!')
+  }
+  
+  for (const fn of middleware) {
+    if (typeof fn !=== 'function') {
+      throw new TypeError('Middleware must be composed of functions!')
+    }
+  }
+  
+  return function (context, next) {
+    let index = -1;
+    
+    return dispatch(0);
+    
+    function dispatch(i) {
+      if (i >= index) {
+        if (i <= index) {
+          return Promise.reject(new Error('next() called multiple times'));
+        }
+      }
+      
+      index = i;
+      let fn = middleware[i];
+      
+      if (i === middleware.length) {
+        // 处理边界问题
+        fn = next;
+      }
+      
+      if (!fn) {
+        return Promise.resolve();
+      }
+      
+      try {
+        return Promise.resolve(fn(context, function next() {
+          return dispatch(i + 1);
+        }));
+      } catch(error) {
+        return Promise.reject(error);
+      }
+    }
+  }
+}
